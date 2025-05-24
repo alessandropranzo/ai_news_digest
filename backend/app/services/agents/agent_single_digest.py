e9d8d57471a5e9735cea8ddd46baff02a36a7ae7
Mattia Martino <m25martino@gmail.com>
2025-05-24 23:48:14 +0200

    single digest agent pipeline

---
+++ backend/app/services/agents/agent_single_digest.py
@@ -0,0 +1,41 @@
+import os
+import signal
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation, ConversationInitiationData
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def client_init(agent_id_name: str, api_key_name: str) -> tuple:
+
+    agent_id = os.getenv(agent_id_name)
+    api_key = os.getenv(api_key_name)
+    elevenlabs = ElevenLabs(api_key=api_key)
+
+    return elevenlabs, agent_id
+
+def conversation_init(elevenlabs: ElevenLabs, agent_id: str, dynamical_vars: dict) -> Conversation:
+    config = ConversationInitiationData(
+        dynamic_variables=dynamical_vars
+    )
+    conversation = Conversation(
+        elevenlabs,
+        agent_id,
+        config=config,
+        audio_interface=DefaultAudioInterface(),
+
+        callback_agent_response=lambda response: print(f"Agent: {response}"),
+        callback_agent_response_correction=lambda original, corrected: print(f"Agent: {original} -> {corrected}"),
+        callback_user_transcript=lambda transcript: print(f"User: {transcript}"),
+    )
+
+    return conversation
+
+def conversation(agent_id_name: str, api_key_name: str, dynamical_vars: dict):
+
+    elevenlabs, agent_id = client_init(agent_id_name, api_key_name)
+    conversation = conversation_init(elevenlabs, agent_id, dynamical_vars)
+
+    conversation.start_session()
+    signal.signal(signal.SIGINT, lambda sig, frame: conversation.end_session())
